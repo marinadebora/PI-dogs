83851876786352d62307a6892076f065637ef480
Debora <m_deby_c@hotmail.com>
2022-06-18 16:12:37 -0300

    commit

---
+++ PI-Dogs-main/api/src/models/Dog.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('dog', {
+    id:{
+      type: DataTypes.UUID, 
+      defaultValue:DataTypes.UUIDV4,
+      allowNull:false,
+      primaryKey:true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    height:{
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    weight:{
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    life_span:{
+      type: DataTypes.STRING
+    }, 
+      createDB:{
+      type:DataTypes.BOOLEAN,
+      allowNull:false,
+      defaultValue:true
+    }
+
+ 
+  },{
+    timestamps: false
+  });
+};
