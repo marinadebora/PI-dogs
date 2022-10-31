c70190ddcad498215e30988259708e1574991f20
Debora <m_deby_c@hotmail.com>
2022-10-31 09:05:38 -0300

    redeploy

--- PI-Dogs-main/api/index.js
+++ PI-Dogs-main/api/index.js
@@ -19,10 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-
+const {
+  DB_PORT
+} = process.env;
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen( DB_PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
