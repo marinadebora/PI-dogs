d01edd6b37abfbbb39c1471f0b2f4403ff966524
Debora <m_deby_c@hotmail.com>
2022-11-15 09:53:40 -0300

    redeploy

--- client/src/action/action.js
+++ client/src/action/action.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
-import dotenv from "dotenv"
-dotenv.config()
 
-axios.defaults.baseURL = "http://localhost:3001"|| process.env.REACT_APP_API 
 
 export function getAllDogs(){
     return async function(dispatch){
