4db712b84146b4a177c6eaa779b2b974cb59b7ab
Debora <m_deby_c@hotmail.com>
2022-11-15 09:25:59 -0300

    migrando el back para deploy

--- client/src/action/action.js
+++ client/src/action/action.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
+import dotenv from "dotenv"
+dotenv.config()
 
+axios.defaults.baseURL = "http://localhost:3001"|| process.env.REACT_APP_API 
 
 export function getAllDogs(){
     return async function(dispatch){
